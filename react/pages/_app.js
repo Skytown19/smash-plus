33dc1f02d8f4600a6298c9a0fe4ef68de1f0ed71
Seth Stevenson <seth@allnovascotia.com>
2020-07-07 08:55:07 -0400

    installed CLSX and copy-pasted some material-ui

--- react/pages/_app.js
+++ react/pages/_app.js
@@ -1,5 +1,12 @@
+import '../css/index.css';
+import NoSsr from '@material-ui/core/NoSsr';
+
 function SmashPlus({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <NoSsr>
+      <Component {...pageProps} />
+    </NoSsr>
+  );
 }
 
 export default SmashPlus;
